fb31cba84c5702748d8aef9d735788a43177f631
johnsloan1984 <johnsloan1984@gmail.com>
2021-03-30 11:26:07 +0100

    tweaked positions and angles for all numbers of participants

--- public/javascript-files/scene/components/camera.js
+++ public/javascript-files/scene/components/camera.js
@@ -11,13 +11,13 @@ window.participants = participants;
 let camera, controls, noP, radius, angle, cameraYPos, cameraZPos, cameraFov, cameraFocY;
 
 export default function setupCamera() {
-	noP = 10
-	let table = includeTable(radius, 32, 0)
+	noP = 5
+	calculatePosRot(noP)
+	let table = includeTable(setupSettings[noP].radius, 32, 0)
 	let column = includeColumn()
 	participants.group = new THREE.Group();
 	participants.group.add( table );
 	participants.group.add( column );
-	calculatePosRot(noP)
 	camera = new THREE.PerspectiveCamera(
 		posRot[noP].camera.fov,
 		window.innerWidth / window.innerHeight,
@@ -38,51 +38,51 @@ const setupSettings = {
 	cameraFocY: 1.59,
 	3: {
 		radius: 0.5,
-		cameraZPos: 0.1,
-		cameraFov: 50,
+		cameraZPos: 0.4,
+		cameraFov: 40,
 		angle: 2*Math.PI/3,
 	},
 	4: {
-		radius: 0.7,
-		cameraZPos: 0.25,
-		cameraFov: 50,
+		radius: 0.65,
+		cameraZPos: 0.5,
+		cameraFov: 40,
 		angle: 2*Math.PI/5,
 	},
 	5: {
 		radius: 0.75,
-		cameraZPos: 0.4,
-		cameraFov: 50,
+		cameraZPos: 0.6,
+		cameraFov: 40,
 		angle: Math.PI/3,
 	},
 	6: {
 		radius: 0.8,
-		cameraZPos: 0.5,
-		cameraFov: 50,
+		cameraZPos: 0.7,
+		cameraFov: 40,
 		angle: 2 * Math.PI/7,
 	},
 	7: {
-		radius: 0.85,
-		cameraZPos: 0.6,
-		cameraFov: 50,
+		radius: 0.9,
+		cameraZPos: 0.85,
+		cameraFov: 40,
 		angle: Math.PI/4,
 	},
 	8: {
-		radius: 0.95,
-		cameraZPos: 0.7,
-		cameraFov: 50,
+		radius: 1,
+		cameraZPos: 0.9,
+		cameraFov: 40,
 		angle: 2 *Math.PI/9,
 	},
 	9: {
 		radius: 1.05,
-		cameraZPos: 0.8,
-		cameraFov: 50,
+		cameraZPos: 0.95,
+		cameraFov: 40,
 		angle: Math.PI/5,
 	},
 	10: {
-		radius: 1.20,
-		cameraZPos: 0.9,
-		cameraFov: 50,
-		angle: 2*Math.PI/11,
+		radius: 1.1,
+		cameraZPos: 1,
+		cameraFov: 40,
+		angle: 3*Math.PI/17,
 	}
 }
 window.setupSettings = setupSettings
