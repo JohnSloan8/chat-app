e44c92ab0f45fd80ee29bec28173ecc82c784af9
johnsloan1984 <johnsloan1984@gmail.com>
2021-04-20 17:02:36 +0100

    got morph to work with tweening

--- public/javascript-files/animations/morph.js
+++ public/javascript-files/animations/morph.js
@@ -8,13 +8,10 @@ window.avatarMorph = avatarMorph
 export default function avatarMorph(who, part, duration) {
 
 	//const expressions = Object.keys( participants[who].movableBodyParts.face.morphTargetDictionary )
-	let partKey = participants[who].movableBodyParts.face.morphTargetDictionary[part]
-	console.log('partKey:', partKey)
 	//console.log('expressions:', expressions)
-	participants[who].movableBodyParts.face.morphTargetInfluences[partKey] = 1;
-	//console.log('face:', face)
-	//const expressions = Object.keys( face.morphTargetDictionary )
-	//let t = new TWEEN.Tween(participants[who]).to(participants[who].rotations[toWhom].head, 0.8*duration)
-	//t.easing(TWEEN.Easing.Quintic.Out)
-	//t.start();
+	let partKey = participants[who].movableBodyParts.face.morphTargetDictionary[part].toString()
+	console.log('partKey:', partKey)
+	let t = new TWEEN.Tween(participants[who].movableBodyParts.face.morphTargetInfluences).to({"40":1}, duration)
+	t.easing(TWEEN.Easing.Quintic.Out)
+	t.start();
 }
