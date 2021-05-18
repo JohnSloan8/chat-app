6cd3a6d77cb1f88e8baa3dd9dd1a0f11e2ae66dc
johnsloan1984 <johnsloan1984@gmail.com>
2021-05-18 12:15:58 +0100

    settings working 2

---
+++ public/javascript-files/scene/settings.js
@@ -0,0 +1,14 @@
+const noParticipants = 6
+const showTable = false
+const showAxesHelper = false
+const showColumn = false
+const showSkeleton = false
+
+export {
+	noParticipants,
+	showTable,
+	showAxesHelper,
+	showColumn,
+	showSkeleton
+}
+
